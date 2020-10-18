30c574f8ca282d373d831175a8dd277bbce752ee
unknown <lukas.hanak@protonmail.com>
2020-10-18 20:59:34 +0200

    basic code for checking if some user wins

--- app.js
+++ app.js
@@ -12,10 +12,9 @@ const allGamingCells = document.getElementsByClassName("gaming-cell");
 for (let i = 0; i < allGamingCells.length; i++) {
     allGamingCells[i].addEventListener('click', () => {
        makeMove(allGamingCells[i].id);
+    //   console.log("Row index: " + allGamingCells[i].closest('tr').rowIndex );
     })
 }
-
-
 
 
 
@@ -27,48 +26,88 @@ function isItDraw() {
 
 }
 
+// make a move, based on the position of the <td> by ID
+var isPlayerOneMoving = true; // or 'O'
+
+function switchPlayer() {
+  if (isPlayerOneMoving === true) {
+    isPlayerOneMoving = false;
+  }
+  else {
+    isPlayerOneMoving = true;
+  }
+}
+
+var player1 = {
+  row1: [],
+  row2: [],
+  row3: [],
+  col1: [],
+  col2: [],
+  col3: [],
+  diag0: [],
+  diag1: [],
+}
+
+var player2 = {
+  row1: [],
+  row2: [],
+  row3: [],
+  col1: [],
+  col2: [],
+  col3: [],
+  diag0: [],
+  diag1: [],
+}
+
+
+function updateBoardInfo(player, cellID) {
+  var rowNumber = document.getElementById(cellID).closest('tr').rowIndex;
+  var colNumber = document.getElementById(cellID).cellIndex;
+  console.log("row number is " + rowNumber);
+  console.log("col numer is " +colNumber);
+}
+
+function makeMove(positionID) {
+  if (document.getElementById(positionID).innerHTML !== "") {
+    console.log("cell is not empty");
+    return 0;
+  }
+  else {
+    let X = '<i class="fa fa-times" aria-hidden="true"></i>';
+    let O = '<span>O</span>';
+
+    // WHEN THE FIRST PLAYER IS ON THE MOVE
+    if (isPlayerOneMoving === true) {
+      document.getElementById(positionID).innerHTML = X;
+      updateBoardInfo(player1, positionID);
+      switchPlayer();
+
+      /*
+      if (singlePlayerMode === true) {
+        makeComputerMove();
+      }
+      */
+    }
+
+    // WHEN THE SECOND PLAYER IS ON THE MOVE
+    else if (isPlayerOneMoving === false) {
+      document.getElementById(positionID).innerHTML = O;
+      updateBoardInfo(player1, positionID);
+      switchPlayer(player2);
+    }
+
+  }
+}
+
+
+
 function resetGamingBoard() {
   for (var i = 0; i < allGamingCells.length; i++) {
     allGamingCells[i].innerHTML="";
   }
 }
 
-// make a move, based on the position of the <td> by ID
-var isPlayerOneMoving = true; // or 'O'
-
-function makeMove(positionID) {
-
-  if (document.getElementById(positionID).innerHTML !== "") {
-    console.log("cell is not empty");
-    return 0;
-  }
-
-  else {
-    let X = '<i class="fa fa-times" aria-hidden="true"></i>';
-    let O = '<span>O</span>';
-
-    if (isPlayerOneMoving === true) {
-      document.getElementById(positionID).innerHTML = X;
-      checkRowWinner(positionID);
-      isPlayerOneMoving = false;
-      if (singlePlayerMode === true) {
-        makeComputerMove();
-      }
-    }
-
-    else if (isPlayerOneMoving === false) {
-      document.getElementById(positionID).innerHTML = O;
-      checkRowWinner(positionID);
-      isPlayerOneMoving = true;
-    }
-
-    else {
-      console.log("start a new game");
-    }
-
-  }
-
-}
 
 function makeComputerMove() {
   let randomNumber = (Math.random() * 8).toFixed(0);
