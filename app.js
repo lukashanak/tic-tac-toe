299410ed76391093a2a83a99b56b647f57beeebd
unknown <lukas.hanak@protonmail.com>
2020-10-18 12:49:14 +0200

    wrote increaseScore function

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
 
-
 /*
 function blinkingAnimation(className) {
   let element = document.getElementsByClassName(className);
@@ -9,31 +8,43 @@ function blinkingAnimation(className) {
 }
 */
 
-function writeXorO(elementID) {
-  let X = '<i class="fa fa-times" aria-hidden="true"></i>';
-  let O = '<span>O</span>';
-  let XorO = "O";
-
-if (document.getElementById(elementID).innerHTML !== "") {
-  console.log("cell not empty!");
-}
-else {
-  if (XorO == "X") {
-      document.getElementById(elementID).innerHTML = X;
-  }
-  if (XorO == "O") {
-    document.getElementById(elementID).innerHTML = O;
-  }
-}
+writeXorO("2");
+const allGamingCells = document.getElementsByClassName("gaming-cell");
+for (let i = 0; i < allGamingCells.length; i++) {
+    allGamingCells[i].addEventListener('click', () => {
+       writeXorO(allGamingCells[i].id);
+       console.log("pica");
+    })
 }
 
-window.onload = function() {
-  writeXorO("2");
+var XorO = "X";
+const PLAYER_ONE_SCORE = document.getElementById("playerOne_score");
+const GAME_NUMBER = document.getElementById("game_number");
+const PLAYER_TWO_SCORE = document.getElementById("playerTwo_score");
 
-  const allGamingCells = document.getElementsByClassName("gaming-cell");
-  for (let i = 0; i < allGamingCells.length; i++) {
-      allGamingCells[i].addEventListener('click', () => {
-         writeXorO(allGamingCells[i].id);
-      })
-  }
+function increaseScore(whichScore) {
+  whichScore.innerHTML = parseInt(whichScore.innerHTML)+1;
 }
+
+
+  function writeXorO(elementID) {
+    let X = '<i class="fa fa-times" aria-hidden="true"></i>';
+    let O = '<span>O</span>';
+
+  if (document.getElementById(elementID).innerHTML !== "") {
+    console.log("cell is not empty");
+  }
+  else {
+    if (XorO == "X") {
+        document.getElementById(elementID).innerHTML = X;
+        XorO = "O";
+    }
+    else if (XorO == "O") {
+      document.getElementById(elementID).innerHTML = O;
+      XorO = "X";
+    }
+    else {
+      console.log("start a new game");
+    }
+  }
+  }
