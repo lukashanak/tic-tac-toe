34df842f937318ebc3d6270d067fc3afc8435d7c
Lukas Hanak <lukas.hanak@protonmail.com>
2020-10-31 00:53:41 +0100

    refactoring part 4

--- app.js
+++ app.js
@@ -1,15 +1,4 @@
-// DOM elements
-const PLAYER_ONE_SCORE = document.getElementById("playerOne_score");
-const GAME_NUMBER = document.getElementById("game_number");
-const PLAYER_TWO_SCORE = document.getElementById("playerTwo_score");
-
-function updateStatistics() {
-  if (isPlayerOneMoving === true) { PLAYER_ONE_SCORE.innerHTML = parseInt(PLAYER_ONE_SCORE.innerHTML)+1; }
-  else { PLAYER_TWO_SCORE.innerHTML = parseInt(PLAYER_TWO_SCORE.innerHTML)+1;}
-  GAME_NUMBER.innerHTML = parseInt(GAME_NUMBER.innerHTML)+1;
-}
-
-// event listener on every cell on the board
+// ADD EVENT LISTENERS ON EVERY CELL ON THE BOARD
 const allGamingCells = document.getElementsByClassName("gaming-cell");
 for (let i = 0; i < allGamingCells.length; i++) {
     allGamingCells[i].addEventListener('click', () => {
@@ -17,22 +6,28 @@ for (let i = 0; i < allGamingCells.length; i++) {
     })
 }
 
-// get info about who is currently playing and switch it if needed
+// DOM ELEMENTS - STATISCTICS
+const PLAYER_ONE_SCORE = document.getElementById("playerOne_score");
+const GAME_NUMBER = document.getElementById("game_number");
+const PLAYER_TWO_SCORE = document.getElementById("playerTwo_score");
+
+// UPDATE STATISTICS - BASED ON WHICH PLAYER IS MOVING
+function updateStatistics() {
+  if (isPlayerOneMoving === true) { PLAYER_ONE_SCORE.innerHTML = parseInt(PLAYER_ONE_SCORE.innerHTML)+1; }
+  else { PLAYER_TWO_SCORE.innerHTML = parseInt(PLAYER_TWO_SCORE.innerHTML)+1;}
+  GAME_NUMBER.innerHTML = parseInt(GAME_NUMBER.innerHTML)+1;
+}
+
+// GET INFO ABOUT WHO IS CURRENTLY PLAYING AND SWITCH IT IF NEEDED
 let isPlayerOneMoving = true;
 let isPlayerTwoMoving = false;
 function switchPlayer() {
-  if (isPlayerOneMoving === true) {
-    isPlayerOneMoving = false;
-    isPlayerTwoMoving = true
-  }
-  else {
-    isPlayerOneMoving = true;
-    isPlayerTwoMoving = false;
-  }
+  isPlayerOneMoving = !isPlayerOneMoving;
+  isPlayerTwoMoving = !isPlayerTwoMoving;
 }
 
 
-// track board info and update it when needed
+// BOARD INFO - USED FOR DETERMINING IF THERE IS A WINNER
 let boardInfo = {
   row0: 0,
   row1: 0,
@@ -44,33 +39,7 @@ let boardInfo = {
   diag1: 0
 }
 
-
-// I hate these two functions, they are messy
-function updateBoardInfo(cellID) {
-  var rowIndex = document.getElementById(cellID).closest('tr').rowIndex;
-  var colIndex = document.getElementById(cellID).cellIndex;
-
-  increaseOrDecreaseNumberByOne(["row"+rowIndex]);
-  increaseOrDecreaseNumberByOne(["col"+colIndex]);
-
-  if (cellID == 0 || cellID == 4 || cellID == 8) {
-    increaseOrDecreaseNumberByOne("diag0");
-  }
-  if (cellID == 2 || cellID == 4 || cellID == 6) {
-    increaseOrDecreaseNumberByOne("diag1");
-  }
-}
-
-function increaseOrDecreaseNumberByOne(property)  {
-  if (isPlayerOneMoving === true) {
-      boardInfo[property]+=1;
-  }
-  else {
-    boardInfo[property]-=1;
-  }
-}
-
-
+// DETERMINE IF THERE IS A WINNER
 function isItWin() {
   for (let key in boardInfo) {
     if (boardInfo.hasOwnProperty(key)) {
@@ -80,6 +49,38 @@ function isItWin() {
     }
 }
 }
+
+// UPDATE THE STATE OF THE BOARDINFO OBJECT
+function updateBoardInfo(cellID) {
+  let rowIndex = document.getElementById(cellID).closest('tr').rowIndex;
+  let colIndex = document.getElementById(cellID).cellIndex;
+
+  increaseOrDecreaseNumberByOne([`row{rowIndex}`]);
+  increaseOrDecreaseNumberByOne([`col{colIndex}`]);
+
+/*
+  if (isPlayerOneMoving === true) {
+
+  }
+  else {
+
+   }
+   */
+
+  if (cellID == 0 || cellID == 4 || cellID == 8) { increaseOrDecreaseNumberByOne("diag0") }
+  if (cellID == 2 || cellID == 4 || cellID == 6) { increaseOrDecreaseNumberByOne("diag1") }
+}
+
+// DECREASE OR INCREASE THE N
+function increaseOrDecreaseNumberByOne(property)  {
+  if (isPlayerOneMoving === true) {
+      boardInfo[property]+=1;
+  }
+  else {
+    boardInfo[property]-=1;
+  }
+}
+
 
 function isCellEmpty(cellID) {
   return document.getElementById(cellID).innerHTML == "";
@@ -114,11 +115,16 @@ function makeMove(cellID) {
        }, 2000);
        return;
      }
-     switchPlayer();
+     switchPlayer()
+     if (singlePlayerMode === true) {
+       makeComputerMove();
+     }
 }
 
 
 function resetGamingBoard() {
+  isPlayerOneMoving = true;
+  isPlayerTwoMoving = false;
   for (var i = 0; i < allGamingCells.length; i++) {
     allGamingCells[i].innerHTML="";
   }
@@ -128,22 +134,20 @@ function resetGamingBoard() {
         boardInfo[key] = 0;
     }
   }
-
 }
 
 
 function makeComputerMove() {
   let randomNumber = (Math.random() * 8).toFixed(0);
-  while(makeMove(randomNumber) == 0) {
+  while(markCell(randomNumber) == 0) {
     randomNumber = (Math.random() * 8).toFixed(0);
   }
 }
 
-var singlePlayerMode = false;
+var singlePlayerMode = true;
 function switchMode() {
-  singlePlayerMode = true;
+  singlePlayerMode = !singlePlayerMode;
 }
-
 
 /*
 function blinkingAnimation(className) {
